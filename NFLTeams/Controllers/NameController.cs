6e189fe7deb98cb9085592481a9a8c41ce991d68
Xavier Hayes <xavier.hayes@southeasttech.edu>
2024-02-11 11:30:09 -0600

    Added Name Controller.

---
+++ NFLTeams/Controllers/NameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NFLTeams.Controllers
+{
+    public class NameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
